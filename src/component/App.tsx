6e9cc0339866e8793c6821f49a8d41a8e5e5d6bf
Gegcbr9319 <gegcbr9319@gmail.com>
2022-11-17 19:01:17 +0300

    refact: delete styles

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Navigate, NavLink, Route, Routes } from 'react-router-dom';
 
-import './App.css';
-import { SignUpPage, SignInPage, MainPage, PageNotFound, BoardsPage } from './page';
+import { SignUpPage, SignInPage, MainPage, PageNotFound, BoardsPage } from '../page';
 
-function App() {
+export function App() {
   return (
     <div className="App">
       <Routes>
@@ -13,7 +12,6 @@ function App() {
         <Route path="/signin" element={<SignInPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/404" element={<PageNotFound />} />
-        <Route path="/*" element={<Navigate to="/404" replace />} />
       </Routes>
     </div>
   );
