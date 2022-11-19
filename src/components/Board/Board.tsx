61989dd787433ba99ceac1ef341ee01329e71ef9
Gegcbr9319 <gegcbr9319@gmail.com>
2022-11-19 23:42:13 +0300

    feat: add style and board component

---
+++ src/components/Board/Board.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Board.module.scss';
+
+export const Board = () => {
+  return (
+    <>
+      <div className={styles.card}>
+        <h3> This card </h3>
+      </div>
+    </>
+  );
+};
--- src/pages/BoardsPage/BoardsPage.tsx
+++ src/pages/BoardsPage/BoardsPage.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Board } from '../../components';
+import styles from './BoardsPage.module.scss';
 
-export function BoardsPage() {
+export const BoardsPage = () => {
+  const [boardsCount, setBoardsCount] = useState(0);
+  const [boardArray, setBoardArray] = useState<number[]>([]);
+  const boardAdd = () => {
+    setBoardsCount(boardsCount + 1);
+    setBoardArray([...boardArray, boardsCount]);
+  };
+  console.log(boardsCount);
+  console.log(boardArray);
   return (
     <>
       <h2>Boards</h2>
+      <button className={styles.button} onClick={boardAdd}>
+        Add Board
+      </button>
+      <div>
+        {boardArray?.map((index) => {
+          return <Board key={index} />;
+        })}
+        <Board />
+      </div>
     </>
   );
-}
+};
