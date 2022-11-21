d130b1b2614a04a7804c314038a951494035e8b0
Yan Malashonak <malashonock@gmail.com>
2022-11-21 16:37:36 +0300

    fix: add authorization header

--- src/api/usersApi.ts
+++ src/api/usersApi.ts
@@ -3,10 +3,13 @@ import { API_BASE } from './constants';
 import { ErrorResponse } from 'model/errors/ErrorResponse';
 import { handleError } from './handleError';
 
-export const getUser = async (id: string): Promise<UserDto> => {
-  const response = (await fetch(`${API_BASE}/users/${id}`).then((response) => response.json())) as
-    | UserDto
-    | ErrorResponse;
+export const getUser = async (id: string, token: string): Promise<UserDto> => {
+  const response = (await fetch(`${API_BASE}/users/${id}`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }).then((response) => response.json())) as UserDto | ErrorResponse;
 
   return handleError<UserDto>(response);
 };
