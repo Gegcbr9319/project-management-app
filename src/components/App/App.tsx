208b48fc58a421db0b7fbb687a429113680a6abb
Stanislau Yarotski <stas_yar@tut.by>
2022-11-22 09:41:05 +0300

    feat: update routing and navigation, add jwt-decode and react-hook-form to dependencies

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,23 +3,54 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { SignUpPage, SignInPage, MainPage, PageNotFound, BoardsPage } from 'pages';
 import { Navigation, Footer } from 'components';
 import styles from './App.module.scss';
+import { PrivateRoute, ProtectedRouter, PublicRoute } from 'specialRoutes';
 
 export function App() {
   return (
-    <>
-      <Navigation />
-      <main className={styles.main}>
-        <Routes>
-          <Route path="/" element={<Navigate to="/main" replace />} />
-          <Route path="/main" element={<MainPage />} />
-          <Route path="/boards" element={<BoardsPage />} />
-          <Route path="/signin" element={<SignInPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/404" element={<PageNotFound />} />
-          <Route path="/*" element={<Navigate to="/404" replace />} />
-        </Routes>
-      </main>
-      <Footer />
-    </>
+    <ProtectedRouter>
+      <>
+        <Navigation />
+        <main className={styles.main}>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <PrivateRoute>
+                  <Navigate to="/boards" replace />
+                </PrivateRoute>
+              }
+            />
+            <Route path="/main" element={<MainPage />} />
+            <Route
+              path="/boards"
+              element={
+                <PrivateRoute>
+                  <BoardsPage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/signin"
+              element={
+                <PublicRoute>
+                  <SignInPage />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="/signup"
+              element={
+                <PublicRoute>
+                  <SignUpPage />
+                </PublicRoute>
+              }
+            />
+            <Route path="/404" element={<PageNotFound />} />
+            <Route path="*" element={<Navigate to="/404" replace />} />
+          </Routes>
+        </main>
+        <Footer />
+      </>
+    </ProtectedRouter>
   );
 }
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import styles from './Navigation.module.scss';
+import { IStore, setToken, Token } from 'store';
 
 export const Navigation = () => {
   const [isSticky, setSticky] = useState(true);
+  const { token } = useSelector((store: IStore) => store);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     window.onscroll = () => {
@@ -15,6 +19,13 @@ export const Navigation = () => {
     };
   }, []);
 
+  const handleSignOut = () => {
+    localStorage.removeItem('token');
+    const { value, time, decoded, isValid, timeout } = new Token();
+    const token = { value, time, decoded, isValid, timeout };
+    dispatch(setToken({ token }));
+  };
+
   return (
     <header className={`${styles.header} ${isSticky ? styles.stickyHeader : ''}`}>
       <nav className={`${styles.navigation} ${isSticky ? styles.stickyNavigation : ''}`}>
@@ -22,15 +33,26 @@ export const Navigation = () => {
           <h1>Project Manager</h1>
         </NavLink>
         <div className={`${styles.links} .links`}>
-          <NavLink className={styles.link} to="/boards">
-            Boards
-          </NavLink>
-          <NavLink className={styles.link} to="/signin">
-            Sign In
-          </NavLink>
-          <NavLink className={styles.link} to="/signup">
-            Sign Up
-          </NavLink>
+          {token.isValid && (
+            <>
+              <NavLink className={styles.link} to="/boards">
+                Boards
+              </NavLink>
+              <button className={styles.link} onClick={handleSignOut}>
+                Sign out
+              </button>
+            </>
+          )}
+          {!token.isValid && (
+            <>
+              <NavLink className={styles.link} to="/signin">
+                Sign In
+              </NavLink>
+              <NavLink className={styles.link} to="/signup">
+                Sign Up
+              </NavLink>
+            </>
+          )}
         </div>
       </nav>
     </header>
