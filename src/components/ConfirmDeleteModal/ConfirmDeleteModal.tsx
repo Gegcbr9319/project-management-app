36f22211459e5ce38977e41db39c833ad452bc0f
Stanislau Yarotski <stas_yar@tut.by>
2022-11-28 06:53:49 +0300

    feat: create ConfirmDeleteModal component, add confirm delete board

---
+++ src/components/ConfirmDeleteModal/ConfirmDeleteModal.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from '@mui/material';
+import { Delete } from '@mui/icons-material';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppState, setDeleteCallback } from 'store';
+import { DeleteState } from 'models';
+
+export const ConfirmDeleteModal = () => {
+  const { callback } = useSelector((state: AppState): DeleteState => state.delete);
+  const dispatch = useDispatch();
+
+  const handleCancel = () => {
+    dispatch(setDeleteCallback(null));
+  };
+
+  const handleDelete = () => {
+    callback && callback();
+    dispatch(setDeleteCallback(null));
+  };
+
+  return (
+    <div>
+      <Dialog
+        open={Boolean(callback)}
+        onClose={handleCancel}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">Внимание!</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Это действие нельзя будет отменить.
+          </DialogContentText>
+          <DialogContentText id="alert-dialog-description">Подтвердить удаление?</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCancel} color="inherit" variant="contained" size="small" autoFocus>
+            Cancel
+          </Button>
+          <Button
+            onClick={handleDelete}
+            variant="contained"
+            startIcon={<Delete />}
+            size="small"
+            color="warning"
+          >
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
