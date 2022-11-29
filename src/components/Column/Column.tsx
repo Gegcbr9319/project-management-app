24125326e440167e35dbe49ae0acc6e05d0de43b
Gegcbr9319 <gegcbr9319@gmail.com>
2022-11-29 22:35:59 +0300

    feat: add button edit and delete

--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useState } from 'react';
 import { IconButton } from '@mui/material';
-import { AddCircle } from '@mui/icons-material';
+import { AddCircle, Delete, Edit } from '@mui/icons-material';
 import styles from './Column.module.scss';
 import { Loader, Modal, Task } from 'components';
 import { useGetTasksInColumnQuery } from 'store';
@@ -22,14 +22,29 @@ export const Column: FC<IColumnProps> = ({ id, title, boardId }) => {
   return (
     <>
       {isLoading && <Loader />}
-      <div>
-        <h3 className={styles.h3}>{title}</h3>
+      <div className={styles.column}>
+        <div className={styles.button}>
+          <IconButton color="warning">
+            <Delete />
+          </IconButton>
+          <h3 className={styles.h3}>{title}</h3>
+          <IconButton color="info" size="small">
+            <Edit />
+          </IconButton>
+        </div>
         <div className={styles.tasks}>
           {data?.map((index) => {
-            return <Task key={index._id} title={index.title} description={index.description} />;
+            return (
+              <Task
+                key={index._id}
+                title={index.title}
+                description={index.description}
+                taskId={index._id}
+              />
+            );
           })}
         </div>
-        <IconButton color="info" aria-label="add an alarm" onClick={tasksAdd}>
+        <IconButton color="info" onClick={tasksAdd} size="large">
           <AddCircle />
         </IconButton>
       </div>
