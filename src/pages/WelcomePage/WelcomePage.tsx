b5b732378966a2b0a75132f7b40689a0c3781815
Stanislau Yarotski <stas_yar@tut.by>
2022-12-05 09:48:27 +0300

    feat: create Welcome Page

--- src/pages/WelcomePage/WelcomePage.tsx
+++ src/pages/WelcomePage/WelcomePage.tsx
@@ -1,116 +1,111 @@
 import React from 'react';
 import styles from './WelcomePage.module.scss';
+import logo from 'assets/svg/logo.svg';
+import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 export function WelcomePage() {
+  const navigate = useNavigate();
+
+  function joinHandler() {
+    navigate('/signup');
+  }
+
+  function enterHandler() {
+    navigate('/signin');
+  }
+
   return (
     <div className={styles.pageWrapper}>
-      <h2>Welcome Page</h2>
-      <p className={styles.p}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab doloribus officiis totam minus
-        reiciendis sed asperiores quis! Dolor temporibus unde voluptatem eos deleniti necessitatibus
-        quisquam? Temporibus maiores ut eaque consectetur facilis dicta, officiis in illum corrupti
-        natus ipsam rem, voluptate quam, exercitationem impedit ea saepe incidunt ipsum ipsa
-        aliquam. Sed, facilis inventore? Esse aliquid delectus cumque cum sint aspernatur quos ullam
-        a mollitia voluptas? Cumque quos quae atque nesciunt officiis tempora et modi quia saepe.
-        Perferendis veritatis beatae quae. Molestias reprehenderit omnis temporibus laboriosam enim
-        impedit maiores, debitis expedita ullam saepe iste sed obcaecati autem rerum sit ea
-        laudantium illo eveniet sint magni ad earum totam mollitia nam? Hic veniam dolorum, in,
-        quidem aliquam voluptates perferendis dolores, ex blanditiis odio libero. Autem alias vero
-        sed natus tenetur inventore consectetur, dolorem optio. Quas modi fuga nisi, doloribus
-        corrupti, perspiciatis excepturi, vitae laboriosam facilis laborum obcaecati delectus
-        repellat eos dolore voluptatem impedit! Magni saepe voluptatum facere. Veniam tempore enim
-        ipsam iure quam? Ad odio doloribus rerum optio maxime nostrum officia facilis commodi in.
-        Laudantium optio dolorem doloribus. Odio vitae iusto adipisci! Ab perferendis minus
-        veritatis explicabo voluptatibus earum! Voluptates non a quam quidem, error neque sapiente
-        minus mollitia esse similique laboriosam? Modi, voluptas sunt, quod, eius quae sapiente
-        perspiciatis architecto alias molestiae aperiam in quos quas? Autem magni nobis error.
-        Ipsum, perferendis quod eveniet voluptates deserunt doloremque vitae molestias ab veritatis!
-        Qui vero beatae esse velit nesciunt asperiores, temporibus rerum veniam quos accusantium
-        quae est doloribus dolore optio facilis odit rem molestiae! Earum magni ullam sapiente
-        ipsam, facilis voluptas, distinctio doloremque maxime, veniam aperiam similique minima a
-        porro. Quibusdam voluptatum itaque fugit esse quis qui perspiciatis quas delectus ipsam
-        accusamus. Consectetur eveniet sequi molestias animi, recusandae harum? Officia labore
-        placeat numquam, laudantium cumque dolore incidunt. Repellendus voluptate, ullam quod
-        delectus voluptas id non exercitationem reiciendis fuga excepturi aliquam adipisci vitae
-        esse beatae est odit laborum modi dolorem nisi sunt obcaecati. Excepturi hic rem similique
-        fugiat culpa? Nulla voluptatum perferendis necessitatibus iusto! Atque a corporis architecto
-        ut! Corrupti, facilis repudiandae. Eum illo laborum consequuntur, assumenda sapiente
-        quisquam inventore dicta quos voluptatibus alias laudantium reprehenderit sunt unde? Sequi
-        enim ad eius voluptatem ipsum in nulla voluptate, veniam, magnam amet, deleniti nihil
-        voluptatibus qui perspiciatis minus blanditiis expedita doloribus! Eius consequatur ex
-        veritatis, architecto eos eum, laborum ducimus omnis explicabo laboriosam consequuntur
-        libero dolores voluptas modi accusamus incidunt natus earum quos minima officiis blanditiis
-        possimus in. Nesciunt doloremque laboriosam voluptas quas non voluptatibus, accusamus dolore
-        facere voluptate officiis ad earum asperiores nobis commodi aliquid neque velit rerum
-        suscipit alias ut illum quo eos mollitia? Voluptas molestiae quae porro sequi sit facere
-        enim sed, voluptate tempore, magnam dolores! Placeat tenetur corporis aspernatur atque
-        doloribus recusandae vitae enim necessitatibus, molestias omnis nostrum sed at reprehenderit
-        cupiditate, obcaecati beatae veritatis commodi facilis! Rerum numquam corporis iure
-        asperiores nisi pariatur adipisci rem sint, dolore consectetur reprehenderit, officiis
-        nobis, minus laboriosam molestiae tempore quaerat vitae iste eaque sequi assumenda?
-        Recusandae quis, incidunt iusto quam voluptatibus laboriosam non placeat labore aspernatur
-        fuga nobis doloribus alias veritatis eaque laborum, delectus libero adipisci id explicabo
-        eos. Quod repudiandae molestiae distinctio odio adipisci veritatis minus doloremque dolore
-        vel mollitia iste quaerat reiciendis ipsum cupiditate odit, laboriosam blanditiis cum
-        suscipit quia pariatur officia porro nostrum nisi perferendis. Aliquid, assumenda vel
-        tenetur quos aliquam aspernatur doloribus et ipsa accusantium, eligendi eos nemo minus natus
-        laborum. Minima reiciendis, molestiae amet corporis exercitationem aspernatur nisi veniam
-        veritatis, odio ea corrupti nulla ipsam repellat aut officiis officia eum. Iste ut
-        doloribus, voluptatibus, sit assumenda ea corporis in non explicabo voluptatum quaerat
-        reiciendis accusantium? Nulla eum itaque voluptas veniam molestias aliquid ullam molestiae
-        perferendis eaque nisi odio explicabo ratione assumenda repellendus aliquam enim eligendi,
-        facilis temporibus harum velit sint voluptatibus neque mollitia! Hic ducimus ut, quo ea
-        dicta non, veniam quibusdam vitae nobis laudantium voluptate esse illum aperiam nulla
-        accusamus provident maxime adipisci similique perspiciatis? Minima voluptas rerum veniam
-        quidem id distinctio nesciunt illo, ex est eveniet dolorum ipsam dolores, odio dicta sunt
-        perspiciatis, fuga velit consectetur adipisci quasi vero voluptates accusantium laudantium.
-        Aperiam expedita, eligendi porro praesentium dignissimos, officia accusantium, non est iusto
-        possimus quis labore. Optio sint iste voluptatibus voluptates molestias explicabo numquam
-        eaque consectetur, ab eos provident similique, debitis architecto itaque esse. Velit alias
-        voluptatem provident aliquam? Accusamus, culpa repellat laudantium, porro eos voluptatum
-        iure, corrupti aliquid et minima temporibus tempora sapiente dolorum! Maiores mollitia
-        commodi dolorum temporibus, recusandae exercitationem minima quam sed maxime, itaque, odit
-        eius fuga! Corrupti ex ut non facere, nemo velit ad reprehenderit rerum eius alias
-        excepturi, possimus accusantium aliquid neque totam magni, at quasi voluptate blanditiis in
-        vitae. Quos accusamus, dicta, porro asperiores ullam totam officiis delectus aut tenetur non
-        possimus molestias dolore est incidunt expedita recusandae ratione aliquid deserunt libero.
-        Perferendis mollitia magnam animi pariatur cupiditate accusamus. Numquam fugiat officia
-        recusandae vel rerum modi harum velit atque possimus asperiores, architecto assumenda,
-        perferendis odit eveniet voluptates quibusdam, deleniti omnis dolore sequi molestiae ipsa.
-        Voluptatibus minima eveniet quae quod laboriosam est dolorem ex voluptate, tempore quidem
-        officiis aliquam vel, illo provident, deleniti vitae. Laudantium necessitatibus, sit
-        molestiae sed, quod minima repudiandae earum corporis numquam tempore saepe iusto ducimus
-        totam ut expedita quas qui quaerat asperiores inventore, veritatis quo vitae cumque? Rerum
-        magnam corporis dolorum vel ipsum ab cupiditate iure, quasi excepturi libero tenetur qui.
-        Pariatur ullam, necessitatibus maxime iste obcaecati reiciendis ex? Assumenda optio nesciunt
-        ad soluta possimus inventore, commodi dolorum praesentium, cumque vero fuga excepturi
-        molestias alias quia quas accusantium dolores? Quasi accusamus quisquam quibusdam aliquam
-        nobis exercitationem magni, deserunt, corrupti est obcaecati adipisci voluptatibus et quos
-        ex ab ad voluptatum? Autem enim eveniet error magni, expedita earum nesciunt doloremque!
-        Fugit necessitatibus odio aliquam recusandae id ea quaerat molestias beatae ducimus eligendi
-        autem tempora, ullam explicabo vitae aliquid, voluptas natus iste accusamus quod illum
-        commodi unde. Voluptatem officiis ipsa rerum recusandae molestias nulla voluptates amet
-        tenetur debitis! Corrupti iure sed fuga voluptate repellat consequatur explicabo magnam a
-        sequi veritatis. Dignissimos facilis, ad et eligendi quas eum, accusamus possimus sunt
-        corporis aperiam ipsa nisi sed doloremque officia dolore? Maiores, dolore? Est eligendi
-        excepturi tempore voluptatum dolorum sequi minima reprehenderit explicabo quidem iusto,
-        reiciendis incidunt. Tenetur aliquam eum, veniam architecto sunt ut vitae, corrupti
-        obcaecati a maiores quod at mollitia nisi fugit harum unde reiciendis. Ad ut consequuntur
-        ullam, pariatur itaque sit excepturi dolorum laboriosam amet minima repudiandae eum
-        perspiciatis quaerat, nam exercitationem numquam quos cumque unde fugiat ea! Autem enim
-        eveniet error magni, expedita earum nesciunt doloremque! Fugit necessitatibus odio aliquam
-        recusandae id ea quaerat molestias beatae ducimus eligendi autem tempora, ullam explicabo
-        vitae aliquid, voluptas natus iste accusamus quod illum commodi unde. Voluptatem officiis
-        ipsa rerum recusandae molestias nulla voluptates amet tenetur debitis! Corrupti iure sed
-        fuga voluptate repellat consequatur explicabo magnam a sequi veritatis. Dignissimos facilis,
-        ad et eligendi quas eum, accusamus possimus sunt corporis aperiam ipsa nisi sed doloremque
-        officia dolore? Maiores, dolore? Est eligendi excepturi tempore voluptatum dolorum sequi
-        minima reprehenderit explicabo quidem iusto, reiciendis incidunt. Tenetur aliquam eum,
-        veniam architecto sunt ut vitae, corrupti obcaecati a maiores quod at mollitia nisi fugit
-        harum unde reiciendis. Ad ut consequuntur ullam, pariatur itaque sit excepturi dolorum
-        laboriosam amet minima repudiandae eum perspiciatis quaerat, nam exercitationem numquam quos
-        cumque unde fugiat ea!
-      </p>
+      <section className={styles.project}>
+        <h2 className={styles.start}>
+          Start your projects with a our Project Manager. Use handy boards, lists, and cards. Manage
+          your projects, organize tasks, and build team spirit—all in one place.
+        </h2>
+        <div className={styles.intro}>
+          <div>
+            <div className={styles.logo}>
+              <img src={logo} alt="logo" />
+              <h2>
+                Project<span>Manager</span>
+              </h2>
+            </div>
+            <p>{` — It’s more than work.`}</p>
+            <p> It’s a way of working together.</p>
+          </div>
+        </div>
+        <div className={styles.buttons}>
+          <Button
+            onClick={enterHandler}
+            variant="contained"
+            className={styles.button}
+            color="inherit"
+          >
+            SIGN IN
+          </Button>
+          <Button onClick={joinHandler} variant="contained" className={styles.button}>
+            SIGN UP
+          </Button>
+        </div>
+      </section>
+      <section className={styles.about}>
+        <h2>Our team</h2>
+        <div className={styles.cards}>
+          <div className={styles.card}>
+            <img
+              src="https://avatars.githubusercontent.com/u/49131247?v=4"
+              width="200"
+              height="200"
+              alt="@badikgit"
+              className={styles.avatar}
+            />
+            <div>
+              <h3>badikgit</h3>
+              <p>
+                Далеко-далеко за словесными, горами в стране гласных и согласных живут рыбные
+                тексты. Собрал деревни коварных продолжил ipsum на берегу решила родного, рукопись
+                семантика свой сих несколько путь мир, гор единственное первую жаренные.
+                Реторический.
+              </p>
+            </div>
+          </div>
+          <div className={styles.card}>
+            <img
+              src="https://avatars.githubusercontent.com/u/95624971?v=4"
+              width="200"
+              height="200"
+              alt="@Gegcbr9319"
+              className={styles.avatar}
+            />
+            <div>
+              <h3>Gegcbr9319</h3>
+              <p>
+                Далеко-далеко за словесными, горами в стране гласных и согласных живут рыбные
+                тексты. Щеке несколько имеет страна, маленький гор они которой, своих о правилами
+                буквенных рекламных раз безорфографичный текст что! Диких, маленькая маленький.
+              </p>
+            </div>
+          </div>
+          <div className={styles.card}>
+            <img
+              src="https://avatars.githubusercontent.com/u/58024256?v=4"
+              width="200"
+              height="200"
+              alt="@malashonock"
+              className={styles.avatar}
+            />
+            <div>
+              <h3>malashonock</h3>
+              <p>
+                Далеко-далеко за словесными, горами в стране гласных и согласных живут рыбные
+                тексты. Обеспечивает первую ты, себя, переписывается строчка образ, встретил текстов
+                не подпоясал взгляд подзаголовок предупредила имени свою продолжил путь! Пор,
+                приставка?
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
